21e0c843a8387ee2e43cd57365f4ae813204e626
Jose <josejesuscedeno@gmail.com>
2022-02-16 18:39:40 +0000

    1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [users, setUsers] =useState([])
+
+	useEffect(() => {
+		fetch("https:gorest.co.in/public/v2/users")
+		.then((response) => response.json())
+		.then((result) => {
+
+		})
+	})
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Usuarios</h1>
 		</div>
 	);
 };
