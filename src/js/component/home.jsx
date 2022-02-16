3fc9ea317d247a2a3d7016d6574fc6e082f9fbbf
Jose <josejesuscedeno@gmail.com>
2022-02-16 19:08:07 +0000

    que pasa

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,12 +8,17 @@ const Home = () => {
 		fetch("https:gorest.co.in/public/v2/users")
 		.then((response) => response.json())
 		.then((result) => {
+			console.log(result);
+			setUsers(result);
+		});
+	}, []);
 
-		})
-	})
 	return (
 		<div>
-			<h1>Usuarios</h1>
+			<h1>Usuarios:</h1>
+			<ul>
+				<li key={1}>{users[1]}</li>;
+			</ul>
 		</div>
 	);
 };
